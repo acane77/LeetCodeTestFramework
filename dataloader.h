b585e5ab580cce39dff25267216eb120f7261a07
miyuki <listencpp@gmail.com>
2021-07-29 08:44:46 +0800

    add LICENSE

--- dataloader.h
+++ dataloader.h
@@ -1,28 +1,19 @@
 /*
- * Leetcode Sample Loader
- *
- * BY MIYUKI,  MARCH, 2021
- * Licensed under GNU General Public License (Version 3)
- *
- * Usage:
- *  ```c++
- *      DataLoader loader;
- *      loader.load(R"sample(
- *            a = [1,2,3, 4]
- *            b=[[1,2,3,4],[4,5,[6,7,8,9]],[]]
- *            c=2.5, d=66
- *            e=["I","love","you"]
- *      )sample");
- *      float c = loader["c"];
- *      int d = loader["d"].asInt();
- *      vector<int> a = loader["a"].asArray<int>();
- *      vector<string> e = loader["e"].asArray<string>();
- *      cout << "c=" << c << endl;
- *      cout << "d=" << d << endl;
- *      cout << "sizeof a=" << a.size() << endl;
- *      cout << "e[1]=" << e[1] << endl;
- *      cout << "b[1][2][3]=" << loader["b"][1][2][3].asInt() << endl;
- *  ```
+    LEETCODE SOLUTION TESTER - Test leetcode cases
+    Copyright (C) 2021  Zhixun Liu
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
  * */
 
 #ifndef DP_DATALOADER_H
